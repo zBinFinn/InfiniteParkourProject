ced7e7ffb3701fd95036085767bc8e6ee3ed90b5
zBinFinn <ermert.finn@icloud.com>
2022-07-12 23:46:05 +0200

    Yes

--- src/main/java/net/finn/test/Main.java
+++ src/main/java/net/finn/test/Main.java
@@ -1,12 +1,23 @@
 package net.finn.test;
 
+import org.bukkit.Bukkit;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.server.ServerListPingEvent;
 import org.bukkit.plugin.java.JavaPlugin;
 
-public final class Main extends JavaPlugin{
+public final class Main extends JavaPlugin implements Listener {
 
     @Override
     public void onEnable() {
-
+        Bukkit.getPluginManager().registerEvents(this, this);
     }
 
+    @EventHandler
+    public void onPing(ServerListPingEvent e) {
+
+        e.
+
+
+    }
 }
